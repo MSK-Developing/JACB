4b646aa04140b4a899cfeeb219f2946c33c80cb3
miky08p <82366841+miky08p@users.noreply.github.com>
2023-02-26 14:45:53 +0100

    Fixed Config

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import discord
 from discord import app_commands
 import string
 
-from devconfig import Config
+from config import Config
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
